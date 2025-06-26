cd34f8e55b8188af4fe6b28f2a870c1d055d0027
AlkimistaRavith <ravith.d3@gmail.com>
2025-06-26 01:13:53 +0000

    desafi completo

---
+++ scripyt.py
@@ -0,0 +1,28 @@
+from usuario import Usuario
+import datetime
+import json
+import pytz
+
+tz_CL = pytz.timezone('America/Santiago')
+datetime_CL = datetime.datetime.now(tz_CL)
+
+lista_usuario = []
+
+with open("usuarios.txt") as u:
+    linea = u.readline()  
+    while linea:
+        try:
+            usuario = json.loads(linea)
+            lista_usuario.append(Usuario(
+                usuario.get("nombre"),
+                usuario.get("apellido"),
+                usuario.get("email"),
+                usuario.get("genero")
+            ))
+        except Exception as e:
+            with open("error.log", "a+") as log:
+                log.write(f"{datetime_CL.strftime("%d/%m/%Y %H:%M:%S")}, {e} \n")
+        finally:
+            linea = u.readline()
+
+print(lista_usuario)
